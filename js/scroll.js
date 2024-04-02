1b7edbc8510f0c038245dd0a3f730dff21583ce3
JeenRibeiroS <jeencarlosjavaweb@gmail.com>
2024-04-02 10:49:49 -0300

    Eviando projeto

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.area-1', {duration: 2000});
+sr.reveal('.area-2', {
+    rotate:{x:0, y:80, z:0},
+    duration: 3000});
+sr.reveal('.area-3', {duration: 4000});
+
+sr.reveal('.price', {
+    rotate:{x:0, y:95, z:0},
+    duration: 2000});
